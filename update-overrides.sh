43be02dab0d1a5f70afbaacc4bc7b6789af43020
Philipp Graf <luxarf@luxarf.de>
2024-04-25 19:40:44 +0200

    Fix override script

--- update-overrides.sh
+++ update-overrides.sh
@@ -4,7 +4,7 @@ set -Eeuo pipefail
 
 OVERRIDES=python-overrides.nix
 
-./generate_package_list.py
+python generate_package_list.py
 
 echo "Generating initial override list as $OVERRIDES"
 pip2nix generate -r InvenTree/requirements.txt --output "$OVERRIDES"
@@ -13,4 +13,4 @@ echo "Initial generation complete, saving copy as ${OVERRIDES}.orig"
 cp "$OVERRIDES" "${OVERRIDES}.orig"
 
 echo "Removing overrides with matching names on nixpkgs"
-./clean_generated_nix.py
+python clean_generated_nix.py
